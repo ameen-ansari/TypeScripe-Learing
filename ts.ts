1440ff4388c22c1b3aea72b5ba6029490d768356
M.Ameen <ameen55668@gmail.com>
2022-08-05 14:31:12 +0500

    initial the folder

---
+++ ts.ts
@@ -0,0 +1,8 @@
+// 1st command (npm i typescript -g)
+// 2nd command (npm i nodemon -g)
+// 3rd command [run in new terminal for translation in JS](tsc app.ts -w)
+// 4th command [For Auto Update the Page in CMD](nodemon app.js)
+
+console.log('hello world')
+var username:string = "ameen";
+console.log(username);
